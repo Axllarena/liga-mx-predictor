7f192367f3ee42ff2254c2a218842dc26ad3c53d
Axllarena <Axllarena@gmail.com>
2026-07-28 22:40:25 -0500

    Match detail arrays

---
+++ src/day01_standings_table.py
@@ -0,0 +1,11 @@
+# Match detail arrays
+home_teams = ["America", "Chivas", "Cruz Azul", "Monterrey", "America", "Monterrey"]
+away_teams = ["Cruz Azul", "Monterrey", "Chivas", "America", "Chivas", "Cruz Azul"]
+home_goals = [2, 0, 3, 1, 2, 2]
+away_goals = [1, 0, 1, 1, 0, 2]
+
+
+
+
+
+
