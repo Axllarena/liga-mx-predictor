ab3be85e5714d72398d0d5ef769d8c0511f51f1f
Axllarena <Axllarena@gmail.com>
2026-08-29 19:33:45 -0600

    Función para describir partido
    Se agregó una funcion que permite realizar la descripción de todos los partidos, permitiendo mejorar la lógica del uso de ciclos, de funciones y de condicionales

--- src/day02_functions.py
+++ src/day02_functions.py
@@ -10,7 +10,20 @@ def get_match_result(home_goals, away_goals):
         return "away_win"
     else:
         return "draw"
+
+    
+#for i in range (len(home_teams)):
+ #   print(get_match_result(home_goals[i], away_goals[i]))
+
+def describe_match(home_team, away_team, home_goals, aways_goals):
+    
+        if (get_match_result(home_goals, aways_goals) == "home_win"):
+          return (f"{home_team} ganó a {away_team} por {home_goals} - {aways_goals}")
+        elif (get_match_result(home_goals, aways_goals) == "away_win"):
+            return(f"{away_team} ganó a {home_team} por {aways_goals} - {home_goals}")
+        else:
+            return(f"{away_team} empató con {home_team} por {aways_goals} - {home_goals}")
+    
     
 for i in range (len(home_teams)):
-    print(get_match_result(home_goals[i], away_goals[i]))
-    
+        print(describe_match(home_teams[i], away_teams[i], home_goals[i], away_goals[i]))
