4bd03523fcbde72f51992eb019b7c359409c2c40
Axllarena <Axllarena@gmail.com>
2026-08-29 20:02:29 -0600

    Added calculate_totals
    Added calculate_totals(home_goals, away_goals), which returns the
    total number of goals scored and total number of draws across all
    matches in the round.

--- src/day02_functions.py
+++ src/day02_functions.py
@@ -15,15 +15,29 @@ def get_match_result(home_goals, away_goals):
 #for i in range (len(home_teams)):
  #   print(get_match_result(home_goals[i], away_goals[i]))
 
-def describe_match(home_team, away_team, home_goals, aways_goals):
-    
-        if (get_match_result(home_goals, aways_goals) == "home_win"):
-          return (f"{home_team} ganó a {away_team} por {home_goals} - {aways_goals}")
-        elif (get_match_result(home_goals, aways_goals) == "away_win"):
-            return(f"{away_team} ganó a {home_team} por {aways_goals} - {home_goals}")
-        else:
-            return(f"{away_team} empató con {home_team} por {aways_goals} - {home_goals}")
-    
-    
-for i in range (len(home_teams)):
-        print(describe_match(home_teams[i], away_teams[i], home_goals[i], away_goals[i]))
+def describe_match(home_team, away_team, home_goals, away_goals):
+
+    if (get_match_result(home_goals, away_goals) == "home_win"):
+        return (f"{home_team} defeated {away_team} {home_goals} - {away_goals}")
+    elif (get_match_result(home_goals, away_goals) == "away_win"):
+        return (f"{away_team} defeated {home_team} {away_goals} - {home_goals}")
+    else:
+        return (f"{away_team} drew with {home_team} {away_goals} - {home_goals}")
+
+for i in range(len(home_teams)):
+    print(describe_match(home_teams[i], away_teams[i], home_goals[i], away_goals[i]))
+
+
+def calculate_totals(home_goals, away_goals):
+    goals = 0
+    draws = 0
+    for i in range(len(home_goals)):
+        if home_goals[i] == away_goals[i]:
+            draws += 1
+        goals += home_goals[i] + away_goals[i]
+    return goals, draws
+
+print("\n")
+totals = calculate_totals(home_goals, away_goals)
+print(f"Total goals scored: {totals[0]}")
+print(f"Total draws: {totals[1]}")
